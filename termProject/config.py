dbc993b16fc57dff0206a354e9c82ff825a61ac9
Robin Charbel Cordahi <125830168+RobinCC24@users.noreply.github.com>
2024-11-17 00:25:10 +0200

    Delete termProject directory

--- termProject/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'mysecretkey'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'  # SQLite for local testing
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
